3acb2054aff4b955a082abcc19783abe2dc7973e
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-06-11 19:54:42 -0300

    ;

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,14 +4,13 @@ export const TodoForm = ({addTodo}) => {
     // criar estado para o nome da tarefa
 
     const handleSubmit = (e) => {
-      e.preventDefault();
       // usar o valo0r e adicionar na função addTodo
     };
 
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
       <input className="todo-input" />
-      <button className='todo-btn'>
+      <button type='button' className='todo-btn'>
 
       </button>
     </form>
